e2eca9511fad656976eff69c32d10dfac353e1a7
gingergaz <gaz@cliffhanger.com>
2021-08-16 11:06:33 +0100

    Add Tile rendering for both appointment and contact info.

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,5 +1,35 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export const Tile = (props) => {
-  return <div className="tile-container">{props.item.name}</div>;
+const Tile = (props) => {
+  let tile = [];
+  /* Codecademy requires first <p> element to have a 'title' class, but there is
+   * no guarantee which property will come first from iteration through an object
+   * - since it also specifies that either appointments OR contacts must be
+   * handled by this Component I need to work around this. I assume 'title' and
+   * 'name' should be first for appointments and contacts respectively
+   */
+  for (let property in props.item) {
+    if (property !== "id") {
+      let tileClass = "tile";
+      if (property === "title" || property === "name") {
+        tileClass += "-title";
+      }
+      tile.push(
+        <p
+          key={`${props.item[property]}${props.item.id}`}
+          className={tileClass}
+        >
+          {props.item[property]}
+        </p>
+      );
+    }
+  }
+  return <div className="tile-container">{tile}</div>;
 };
+
+Tile.propTypes = {
+  item: PropTypes.object.isRequired,
+};
+
+export default Tile;
--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Tile } from "../tile/Tile";
+import Tile from "../tile/Tile";
 
 const TileList = (props) => {
   const tileArray = props.arr.map((item) => <Tile item={item} key={item.id} />);
